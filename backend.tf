99e3beeb3022182bc7671a9f6d06f5b4f07cd1cf
rohit <rohit27171757@gmail.com>
2025-04-26 17:10:48 +0530

    dynamodb-creation

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "my-terraform-state-bucket87"
     key            = "env/dev/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "terraform-locks"
