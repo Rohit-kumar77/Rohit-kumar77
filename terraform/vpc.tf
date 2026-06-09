8afca73405406ffc27e35178128c5fddf532ceeb
Rohit Kumar <Rohitops007@gmail.com>
2026-06-09 16:32:07 +0530

    terraform first-bucket project

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -17,12 +17,12 @@ resource "aws_vpc" "demo_vpc"{
 
 resource "aws_subnet" "public_subnet"{
     vpc_id= aws_vpc.demo_vpc.id
-    cidr_block = "10.10.0.0/24"    
+    cidr_block = "10.0.0.0/24"    
 }
 
 resource "aws_subnet" "private_subnet" {
     vpc_id= aws_vpc.demo_vpc.id
-    cidr_block = "10.10.1.0/24"
+    cidr_block = "10.0.1.0/24"
 }
 
 resource "aws_internet_gateway" "igw"{
