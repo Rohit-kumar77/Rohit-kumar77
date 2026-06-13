8229fbd66b72366800e487fe213655b114db28c9
Rohit Kumar <Rohitops007@gmail.com>
2026-06-13 17:25:51 +0530

    VPC example commit 3

--- terraform/06-resources/networking.tf
+++ terraform/06-resources/networking.tf
@@ -20,6 +20,26 @@ resource "aws_subnet" "pubic"{
     }
 }
 
-provider "aws" {
-    region = "us-east-2"
+resource "aws_internet_gateway" "main" {
+    vpc_id = aws_vpc.main.vpc_id
+
+    tags = {
+        Name  = "06-resource-main"
+        ManagedBy = "Terraform"
+        Project   = "06-resource"
+    }
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.vpc_id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.vpc_id
+
+    }
+}
+
+resource "aws_route_table_association" "public"{
+    subnet_id = aws_subnet.pubic.vpc_id
+    route_table_id = aws_route_table.public.id 
 }
