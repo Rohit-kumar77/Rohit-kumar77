5b4933d5fd7697f7f104ceefbda8f003b8e1c151
Rohit Kumar <Rohitops007@gmail.com>
2026-06-13 17:06:10 +0530

    VPC example commit 1

---
+++ terraform/06-resources/networking.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc" "dev"{
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+        Name      = "vpc-test"
+        ManagedBy = "Terraform"
+        Project = "Terraform"
+
+    }
+}
+
+resource "aws_subnet" "pubic"{
+    vpc_id = aws.vpc.main.vpc_id
+    cidr_block = "10.0.0.0/24"
+
+    tags = {
+        Name  = "06-resource"
+        ManagedBy = "Terraform"
+        Project   = "06-resource"
+    }
+}
