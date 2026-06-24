5faf830a9b59770c10339d718472882f2a566ed3
Rohit Kumar <Rohitops007@gmail.com>
2026-06-25 00:22:29 +0530

    data source

---
+++ data-source/compute.tf
@@ -0,0 +1,35 @@
+data "aws_ami" "ubuntu" {
+
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    filter {
+        name = "region"
+        values = ["us-east-1"]
+    }
+    owners = ["099720109477"]
+}
+
+output "test" {
+  value = data.aws_ami.ubuntu
+}
+   
+
+#data "aws_caller_identity" "current"{ }
+
+#data "aws_region" "current" {
+#    provider = aws.us_east
+#}
+
+#output "ubuntu_ami_data" {
+#    value = data.aws_ami.ubuntu.id
+#}
